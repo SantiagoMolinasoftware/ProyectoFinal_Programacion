93e4e27d70efcd04187f1d45fd0b0c1ba512c451
SantiagoMolinasoftware <146997008+SantiagoMolinasoftware@users.noreply.github.com>
2024-01-28 13:02:27 -0500

    Update pruebas.c

--- pruebas.c
+++ pruebas.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define Horas_Semanales 40
-#define RECARGO_EXTRA_50 1.5
-#define RECARGO_FERIADO 2.0
+#define HORAS_SEMANALES 40
+#define RECARGO_EXTRA_DIURNO 1.5
+#define RECARGO_FERIADO_DIURNO 2.0
 
-typedef struct {
-    char nombre[50];
-    double salarioMensual;
-    int tiempoDescanso;
-    int horaInicio;
-    int minutoInicio;
-    int horaFin;
-    int minutoFin;
-    int trabajoFeriado;
-    int horasExtras;
+typedef struct Empleado {
+    char nombreCompleto[50];
+    double salarioMensualUSD;
+    int tiempoDescansoMinutos;
+    int horaInicioDiurna;
+    int minutoInicioDiurno;
+    int horaFinDiurna;
+    int minutoFinDiurno;
+    int trabajoFeriadoBooleano;
+    int horasExtrasDiurnas;
 } Empleado;
 
-void solicitarDatos(Empleado *empleado);
-void mostrarDatos(Empleado empleado);
-int calcularHorasTrabajadas(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso);
-double calcularValorHora(double salarioMensual, int tiempoDescanso);
-void calcularHorasExtras(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso, double valorHora, int trabajoFeriado, int *horasExtras, double *valorExtra, double *valorSuplementario);
-void guardarEmpleado(Empleado empleado);
-void mostrarSueldoMensual();
-void limpiarSistema();
+void solicitar_datos(Empleado *empleado);
+void guardar_empleado(Empleado empleado);
+void mostrar_sueldo_mensual();
+void limpiar_sistema();
+int calcular_horas_trabajadas(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso);
+double calcular_valor_hora(double salarioMensual, int tiempoDescanso);
+void calcular_horas_extras(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso, int *horasExtras);
 
 int main() {
     int opcion;
@@ -41,19 +41,19 @@ int main() {
             case 1:
                 printf("\nRegistro de empleado:\n");
                 Empleado empleado;
-                solicitarDatos(&empleado);
-                guardarEmpleado(empleado);
+                solicitar_datos(&empleado);
+                guardar_empleado(empleado);
                 break;
             case 2:
                 printf("\nSueldo mensual de los empleados:\n");
-                mostrarSueldoMensual();
+                mostrar_sueldo_mensual();
                 break;
             case 3:
                 printf("\n¿Está seguro de limpiar el sistema? (1 para sí, 0 para no): ");
                 int confirmacion;
                 scanf("%d", &confirmacion);
                 if (confirmacion == 1) {
-                    limpiarSistema();
+                    limpiar_sistema();
                     printf("El sistema ha sido limpiado.\n");
                 } else {
                     printf("Operación cancelada.\n");
@@ -70,24 +70,24 @@ int main() {
     return 0;
 }
 
-void solicitarDatos(Empleado *empleado) {
+void solicitar_datos(Empleado *empleado) {
     printf("Ingrese el nombre del empleado: ");
-    scanf("%s", empleado->nombre);
+    scanf("%s", empleado->nombreCompleto);
     printf("Ingrese el salario mensual (USD): ");
-    scanf("%lf", &empleado->salarioMensual);
+    scanf("%lf", &empleado->salarioMensualUSD);
     printf("Ingrese el tiempo de almuerzo o descanso dentro de la jornada de trabajo (Minutos): ");
-    scanf("%d", &empleado->tiempoDescanso);
+    scanf("%d", &empleado->tiempoDescansoMinutos);
     printf("Ingrese la hora de inicio de labores (hh:mm): ");
-    scanf("%d:%d", &empleado->horaInicio, &empleado->minutoInicio);
+    scanf("%d:%d", &empleado->horaInicioDiurna, &empleado->minutoInicioDiurno);
     printf("Ingrese la hora de fin de labores (hh:mm): ");
-    scanf("%d:%d", &empleado->horaFin, &empleado->minutoFin);
-    printf("Ha trabajado en feriados? (1 para si, 0 para no): ");
-    scanf("%d", &empleado->trabajoFeriado);
+    scanf("%d:%d", &empleado->horaFinDiurna, &empleado->minutoFinDiurno);
+    printf("Ha trabajado en feriados? (1 para sí, 0 para no): ");
+    scanf("%d", &empleado->trabajoFeriadoBooleano);
     printf("Ha trabajado horas extras? (Ingrese el numero de horas extras): ");
-    scanf("%d", &empleado->horasExtras);
+    scanf("%d", &empleado->horasExtrasDiurnas);
 }
 
-void guardarEmpleado(Empleado empleado) {
+void guardar_empleado(Empleado empleado) {
     FILE *archivo;
     archivo = fopen("empleados.txt", "a");
     if (archivo == NULL) {
@@ -95,13 +95,13 @@ void guardarEmpleado(Empleado empleado) {
         exit(1);
     }
 
-    fprintf(archivo, "%s %.2lf %d %d %d %d %d %d %d\n", empleado.nombre, empleado.salarioMensual, empleado.tiempoDescanso,
-            empleado.horaInicio, empleado.minutoInicio, empleado.horaFin, empleado.minutoFin, empleado.trabajoFeriado, empleado.horasExtras);
+    fprintf(archivo, "%s %.2lf %d %d %d %d %d %d %d %d\n", empleado.nombreCompleto, empleado.salarioMensualUSD, empleado.tiempoDescansoMinutos,
+            empleado.horaInicioDiurna, empleado.minutoInicioDiurno, empleado.horaFinDiurna, empleado.minutoFinDiurno, empleado.trabajoFeriadoBooleano, empleado.horasExtrasDiurnas);
 
     fclose(archivo);
 }
 
-void mostrarSueldoMensual() {
+void mostrar_sueldo_mensual() {
     FILE *archivo;
     archivo = fopen("empleados.txt", "r");
     if (archivo == NULL) {
@@ -109,34 +109,29 @@ void mostrarSueldoMensual() {
         exit(1);
     }
 
-    printf("\n%-20s %-20s %-20s\n", "Nombre", "Sueldo Mensual", "Detalles");
+    printf("\n%-20s %-20s %-20s\n", "Nombre", "Sueldo Mensual", "Horas Extras");
     printf("-------------------------------------------------------------\n");
 
     Empleado empleado;
-    while (fscanf(archivo, "%s %lf %d %d %d %d %d %d %d\n", empleado.nombre, &empleado.salarioMensual, &empleado.tiempoDescanso,
-            &empleado.horaInicio, &empleado.minutoInicio, &empleado.horaFin, &empleado.minutoFin, &empleado.trabajoFeriado, &empleado.horasExtras) != EOF) {
+    while (fscanf(archivo, "%s %lf %d %d %d %d %d %d %d %d\n", empleado.nombreCompleto, &empleado.salarioMensualUSD, &empleado.tiempoDescansoMinutos,
+            &empleado.horaInicioDiurna, &empleado.minutoInicioDiurno, &empleado.horaFinDiurna, &empleado.minutoFinDiurno, &empleado.trabajoFeriadoBooleano, &empleado.horasExtrasDiurnas) != EOF) {
         
-        double valorHora = calcularValorHora(empleado.salarioMensual, empleado.tiempoDescanso);
-        double valorExtra, valorSuplementario;
-        calcularHorasExtras(empleado.horaInicio, empleado.minutoInicio, empleado.horaFin, empleado.minutoFin, empleado.tiempoDescanso,
-                            valorHora, empleado.trabajoFeriado, &empleado.horasExtras, &valorExtra, &valorSuplementario);
-
-        double sueldoMensual = empleado.salarioMensual + valorExtra + valorSuplementario;
+        double valor_hora = calcular_valor_hora(empleado.salarioMensualUSD, empleado.tiempoDescansoMinutos);
+        int horas_extras = empleado.horasExtrasDiurnas;
         
-        // Detalles de horas extras y trabajo en feriados
-        char detalles[100];
-        if (empleado.trabajoFeriado)
-            sprintf(detalles, "Horas Extras: %d, Trabajo en Feriado", empleado.horasExtras);
-        else
-            sprintf(detalles, "Horas Extras: %d", empleado.horasExtras);
-
-        printf("%-20s %-20.2lf %-20s\n", empleado.nombre, sueldoMensual, detalles);
+        double sueldo_mensual = empleado.salarioMensualUSD;
+        if (empleado.trabajoFeriadoBooleano)
+            sueldo_mensual += empleado.salarioMensualUSD / 30 * RECARGO_FERIADO_DIURNO;
+        
+        sueldo_mensual += horas_extras * valor_hora * RECARGO_EXTRA_DIURNO;
+        
+        printf("%-20s %-20.2lf %-20d\n", empleado.nombreCompleto, sueldo_mensual, horas_extras);
     }
 
     fclose(archivo);
 }
 
-void limpiarSistema() {
+void limpiar_sistema() {
     FILE *archivo;
     archivo = fopen("empleados.txt", "w");
     if (archivo == NULL) {
@@ -146,40 +141,30 @@ void limpiarSistema() {
     fclose(archivo);
 }
 
-int calcularHorasTrabajadas(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso) {
-    int minutosInicioTotal = horaInicio * 60 + minutoInicio;
-    int minutosFinTotal = horaFin * 60 + minutoFin;
-    int minutosTrabajados = minutosFinTotal - minutosInicioTotal - tiempoDescanso;
+int calcular_horas_trabajadas(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso) {
+    int minutos_inicio_total = horaInicio * 60 + minutoInicio;
+    int minutos_fin_total = horaFin * 60 + minutoFin;
+    int minutos_trabajados = minutos_fin_total - minutos_inicio_total - tiempoDescanso;
     
-    if (minutosTrabajados < 0) {
-        minutosTrabajados += 24 * 60;
+    if (minutos_trabajados < 0) {
+        minutos_trabajados += 24 * 60;
     }
     
-    return minutosTrabajados / 60;
+    return minutos_trabajados / 60;
 }
 
-double calcularValorHora(double salarioMensual, int tiempoDescanso) {
-    int horasLaborables = Horas_Semanales * 4 - (tiempoDescanso / 60 * 5); 
-    return salarioMensual / horasLaborables;
+double calcular_valor_hora(double salarioMensual, int tiempoDescanso) {
+    int horas_laborables = HORAS_SEMANALES * 4 - (tiempoDescanso / 60 * 5); 
+    return salarioMensual / horas_laborables;
 }
 
-void calcularHorasExtras(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso, double valorHora, int trabajoFeriado,
-                         int *horasExtras, double *valorExtra, double *valorSuplementario) {
-    int horasTrabajadas = calcularHorasTrabajadas(horaInicio, minutoInicio, horaFin, minutoFin, tiempoDescanso);
-    int horasNormales = Horas_Semanales * 4 - (tiempoDescanso / 60 * 5);
+void calcular_horas_extras(int horaInicio, int minutoInicio, int horaFin, int minutoFin, int tiempoDescanso, int *horasExtras) {
+    int horas_trabajadas = calcular_horas_trabajadas(horaInicio, minutoInicio, horaFin, minutoFin, tiempoDescanso);
+    int horas_normales = HORAS_SEMANALES * 4 - (tiempoDescanso / 60 * 5);
     
-    if (horasTrabajadas > horasNormales) {
-        *horasExtras = horasTrabajadas - horasNormales;
-        *valorExtra = (*horasExtras * valorHora * RECARGO_EXTRA_50) / 2;
-        *valorSuplementario = (*horasExtras * valorHora * RECARGO_EXTRA_50) / 2;
-    } else {
+    *horasExtras = horas_trabajadas - horas_normales;
+    if (*horasExtras < 0) {
         *horasExtras = 0;
-        *valorExtra = 0;
-        *valorSuplementario = 0;
-    }
-
-    if (trabajoFeriado) {
-        *valorExtra += horasTrabajadas * valorHora * RECARGO_FERIADO;
     }
 }
 
